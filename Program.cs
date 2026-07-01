0ab564b81f8d51f6f10cb5506d9c69a3ac3b463f
Sevaprilukov <sevaprilukov@example.com>
2026-07-01 14:11:42 +0300

    готовая программа 2

--- Program.cs
+++ Program.cs
@@ -6,16 +6,12 @@ class Program
     {
         Console.Write("Введите рост (в метрах): ");
         double height = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
-
         Console.Write("Введите вес (в килограммах): ");
         double weight = Convert.ToDouble(Console.ReadLine().Replace('.', ','));
 
         //формулу и данные я брал с сайта https://www.vidal.ru/calculators/kardiologiya/indeks-massy-tela?ysclid=mr1yzhik6780082875
-
         double bmi = weight / (height * height);
-
         Console.Write("ИМТ = " + Math.Round(bmi, 1) + " - ");
-
         if (bmi < 18.5)
         {
             Console.WriteLine("Ниже нормального");
